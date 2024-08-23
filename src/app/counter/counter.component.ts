a558c6b0f0b94a40f27f15bd8426d7d2fff2f27e
Kojoel <nanaabruquah1s@gmail.com>
2024-08-23 13:12:38 +0000

    Update to Counter UI

--- src/app/counter/counter.component.ts
+++ src/app/counter/counter.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+import { AppState } from '../states/app.state';
+import { Store } from '@ngrx/store';
+import { selectCount } from '../states/counter.selector';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-counter',
   standalone: true,
-  imports: [],
+  imports: [AsyncPipe],
   templateUrl: './counter.component.html',
   styleUrl: './counter.component.scss'
 })
 export class CounterComponent {
+  count$: Observable<number> | undefined;
 
+  constructor(private store: Store<AppState>){
+    this.count$ = this.store.select(selectCount)
+  }
 }
